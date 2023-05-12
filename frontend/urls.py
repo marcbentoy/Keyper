98cb2e7c74076e0c320bd27610c24a20d92d9307
Harrah <serafinharrah@gmail.com>
2023-05-12 17:15:42 +0800

    update: about

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index')
 ]
+urlpatterns += staticfiles_urlpatterns()
